c7b52171fa70f3da154888150cb67d6473560a05
Ram Mittala <mittalas@amazon.com>
2020-01-14 10:30:06 -0800

    Update build configuration.

--- utilities/src/main/java/com/ram/utils/PersonApp.java
+++ utilities/src/main/java/com/ram/utils/PersonApp.java
@@ -1,7 +1,17 @@
 package com.ram.utils;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 public class PersonApp {
     public static void main(String[] args) {
+        
         Person.builder().build();
+        try {
+            ThreadMXBean threadMXBean =  ManagementFactory.getThreadMXBean();
+            System.out.println(threadMXBean.getClass().getName());
+        } catch(Exception ex ){
+        
+        }
     }
 }
